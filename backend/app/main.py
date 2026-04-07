dd4269b263552278fc5fb840ba3eb8e88c705c75
Diya Ghosh <diya1692005ne@gmail.com>
2026-04-07 20:32:22 +0530

    Fix import statement for router in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes.api import router
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from .routes.api import router
 
 app = FastAPI()
 
