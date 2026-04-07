47bef888c4b7f114de5609d06bc11743f86c6b60
Diya Ghosh <diya1692005ne@gmail.com>
2026-04-07 20:52:49 +0530

    Update hybrid_summarizer.py

--- backend/app/services/hybrid_summarizer.py
+++ backend/app/services/hybrid_summarizer.py
@@ -5,7 +5,16 @@ from sentence_transformers import SentenceTransformer
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity
 
-nltk.download('punkt')
+import os
+
+nltk_data_path = "/opt/render/nltk_data"
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt", download_dir=nltk_data_path)
 model = SentenceTransformer('all-mpnet-base-v2') 
 
 def sentence_split(text):
