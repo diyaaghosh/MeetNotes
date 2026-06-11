defa19fb31c883db73866240d47adeeb511e591d
Diya Ghosh <diya1692005ne@gmail.com>
2026-06-11 23:16:12 +0530

    update

--- backend/app/routes/api.py
+++ backend/app/routes/api.py
@@ -1,29 +1,20 @@
 from fastapi import APIRouter, UploadFile, File
 import shutil
 import os
-from ..services.hybrid_summarizer import summarize
-from ..services.extractor import convert_to_bullets
+from ..services.hybrid_summarizer import summarize, extract_key_points
 from fastapi.responses import FileResponse
 from ..services.pdf_service import generate_pdf
+
 router = APIRouter()
-# @router.post("/process-text")
-# async def process_text(data: dict):
-#     text = data["text"]
 
-#     summary = summarize(text)
-#     bullets = convert_to_bullets(summary)
 
-#     return {
-#         "summary": summary,  
-#         "bullets": bullets
-#     }
 @router.post("/process_text")
 async def process_text(data: dict):
     try:
         text = data["text"]
 
         summary = summarize(text)
-        bullets = convert_to_bullets(summary)
+        bullets = extract_key_points(summary)
 
         return {
             "summary": summary,
@@ -40,12 +31,15 @@ async def process_text(data: dict):
             "error": str(e),
             "traceback": error
         }
+
+
 @router.post("/generate-pdf")
 def create_pdf(data: dict):
     summary = data.get("summary", [])
     bullets = data.get("bullets", [])
 
     output_file = "output.pdf"
+
     generate_pdf(summary, bullets, output_file)
 
     return FileResponse(
